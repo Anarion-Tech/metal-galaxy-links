8e81c2168755dbde53e3a2ca67143c436f88578d
Anarion <pro@anarion.tech>
2026-01-30 15:24:45 +0100

    font stuff again

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,12 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -59,8 +64,8 @@ export default {
         },
       },
       fontFamily: {
-        sans: ['Oswald', 'system-ui', 'sans-serif'],
-        metal: ['Metal Mania', 'cursive'],
+        sans: ["Inter", "system-ui", "sans-serif"],
+        metal: ["Inter", "system-ui", "sans-serif"],
       },
       borderRadius: {
         lg: "var(--radius)",
